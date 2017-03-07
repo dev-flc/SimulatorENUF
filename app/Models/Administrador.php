ccd041811618fee05b45d299f98ff224582aede5
fernando <fer.lc.developer@gmail.com>
2017-03-07 17:24:36 -0600

    create model admin

--- app/Models/Administrador.php
+++ app/Models/Administrador.php
@@ -6,5 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Administrador extends Model
 {
-    //
+    protected $table = 'administradors';
+	protected $primaryKey = 'ADM_id';
+    protected $fillable = [
+        'ADM_nombre',
+        'ADM_apellido_p',
+        'ADM_apellido_m',
+        'ADM_sexo',
+        'USE_id',
+        'DIR_id'
+    ];
 }
--- app/Models/Alumno.php
+++ app/Models/Alumno.php
@@ -6,5 +6,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class Alumno extends Model
 {
-    //
+    protected $table = 'alumnos';
+	protected $primaryKey = 'ADM_id';
+    protected $fillable = [
+        'ALU_nombre',
+        'ALU_apellido_p',
+        'ALU_apellido_m',
+        'ALU_edad',
+        'ALU_sexo',
+        'ALU_matricula',
+        'USE_id',
+        'DIR_id',
+        'CUR_id'
+    ];
 }
