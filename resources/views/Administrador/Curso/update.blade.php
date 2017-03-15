9631f1499335788159b6b99f0b4d9667516068ac
fernando <fer.lc.developer@gmail.com>
2017-03-15 00:27:17 -0600

    create style

--- resources/views/Administrador/Curso/update.blade.php
+++ resources/views/Administrador/Curso/update.blade.php
@@ -13,25 +13,29 @@
     <li><a href="{{ route('profesores.create') }}">Create profesores</a></li>
   </ul>
 	{!! Form::open(['route' => ['cursos.update', $curso->CUR_id],'method' => 'put']) !!}
-	<div class="form-group">
-    	{!! Form::label('nombre','Nombre') !!}
+    <div class="form-group">
+        {!! Form::label('nombre','Nombre') !!}
         {!! Form::text('nombre',$curso->CUR_nombre,['class'=>'form-control','required'])!!}
     </div>
     <div class="form-group">
-    	{!! Form::label('cupos','Cupos') !!}
+        {!! Form::label('cupos','Cupos') !!}
         {!! Form::number('cupos',$curso->CUR_cupos,['class'=>'form-control','required'])!!}
     </div>
-	<label for="profesor">Profesor</label>
-    <select  name="profesor" class="form-control" required>
-        <option value="">Seleccione un profesor</option>
+    <label for="profesor">Profesor</label>
+    <select  name="asesor" class="form-control" required>
+        <option value="{{ $profesor->PRO_id}}">{{ $profesor->PRO_nombre }} {{ $profesor->PRO_apellido_p }} {{ $profesor->PRO_apellido_m }}*</option>
         @foreach ($profesores as $pro)
+        @if($profesor->PRO_id==$pro->PRO_id)
+
+        @else if
         <option value="{{ $pro->PRO_id}}">{{ $pro->PRO_nombre}} {{ $pro->PRO_apellido_p}} {{ $pro->PRO_apellido_m}} </option>
+        @endif
         @endforeach
     </select>
 
 
-	   <div class="form-group">
-    {{ Form::button('<span class="glyphicon glyphicon-ok"></span> Actualizar', array('class'=>'btn btn-success pull-right', 'type'=>'submit')) }}
+  <div class="form-group">
+    {{ Form::button('<span class="glyphicon glyphicon-ok"></span> Registrar', array('class'=>'btn btn-success pull-right', 'type'=>'submit')) }}
     </div>
 
 
--- resources/views/Profesor/Curso/show.blade.php
+++ resources/views/Profesor/Curso/show.blade.php
@@ -20,7 +20,8 @@
 @section('content')
 <div class="container-fluid">
 <div class="row">
-  <div class="col-sm-12"><center><h1>{{ $curso->CUR_nombre}}</h1></center></div>
+  <div class="col-sm-12"><center><h1>{{ $curso->CUR_nombre}}</h1></center><br></div>
+
   <div class="col-sm-4">
     <center>
       <p id="centradop">Cupos del curso</p>
@@ -46,42 +47,106 @@
       <p id="centradop">Fecha</p>
       <h2>
         <span class="label label-default">
-          #{{ $curso->CUR_fecha}}
+          {{ $curso->CUR_fecha}}
         </span>
       </h2>
     </center>
   </div>
 </div>
 <hr>
+<style type="text/css">
+  #alumnosimg
+  {
+    width: 50px;
+    height: 50px;
+    border-radius: 50%;
+    margin: 5px;
+  }
+  .cuadro
+  {
+    border-radius: 6px;
+    background: rgb(242, 243, 244);
+
+  }
+  #textcenter
+  {
+    margin: 5px;
+    text-align: center;
+    font-size: 17px;
+  }
+  .aprobada
+  {
+    color: rgb(72, 201, 176);
+    font-size: 17px;
+  }
+  .pendiente
+  {
+    color: rgb(236, 112, 99);
+    font-size: 17px;
+  }
+</style>
 <div class="row">
   <div class="col-sm-6">
+  <div class="container-fluid">
     <center><h2>Alumnos</h2></center>
+    <div class="row">
+      <div class="col-sm-12 cuadro">
+        <div class="col-sm-2">
+          <img id="alumnosimg" src="http://www.imagenes-bonitas.net/wp-content/uploads/2013/12/fotos-de-caras-bonitas-1024x819.jpg" alt="">
+        </div>
+        <div class="col-sm-6">
+         <center> <p id="textcenter">Fernando Lucena Calixto hernandez</p></center>
+        </div>
+        <div class="col-sm-4"><br>
+          <span class="glyphicon glyphicon-star aprobada" aria-hidden="true"></span>
+          <span class="glyphicon glyphicon-star aprobada" aria-hidden="true"></span>
+          <span class="glyphicon glyphicon-star-empty pendiente" aria-hidden="true"></span>
+          <span class="glyphicon glyphicon-star-empty pendiente" aria-hidden="true"></span>
+          <span class="glyphicon glyphicon-star-empty pendiente" aria-hidden="true"></span>
+        </div>
+      </div>
+    </div>
+  </div>
   </div>
   <div class="col-sm-6">
-    <center><h2>Preguntas</h2></center>
+  <div class="container-fluid">
+    <center><h2>Unidades</h2></center>
+    <div class="row">
+      <div class="col-sm-12 cuadro">
+        <div class="col-sm-4">
+          <img id="alumnosimg" src="http://www.imagenes-bonitas.net/wp-content/uploads/2013/12/fotos-de-caras-bonitas-1024x819.jpg" alt="">
+        </div>
+        <div class="col-sm-8">
+
+        </div>
+      </div>
+    </div>
+  </div>
   </div>
 </div>
 </div>
   <br>
   <br>
   <br>
+
 <div class="contenedor">
 <button class="botonF1">
   <span>+</span>
 </button>
-<button class="btn-m botonF2" data-toggle="modal" data-target="#login">
+<button class="btn-m botonF2 tooltip-right tooltip-right1" data-toggle="modal" data-target="#alumno"  data-tooltip="Nuevo Alumno">
+  <span class="glyphicon glyphicon-user" aria-hidden="true" style="font-size: 18px"></span>
+</button>
+<button class="btn-m botonF3 tooltip-right tooltip-right2" data-toggle="modal" data-target="#unidad"  data-tooltip="Nueva Unidad">
+  <span class="glyphicon glyphicon-book" aria-hidden="true" style="font-size: 18px"></span>
+</button>
+<!--
+<button class="btn-m botonF4 tooltip-right tooltip-right3" data-toggle="modal" data-target="#login"  data-tooltip="ffff">
   <span>+</span>
 </button>
-<button class="btn-m botonF3">
+<button class="btn-m botonF5 tooltip-right tooltip-right4" data-toggle="modal" data-target="#login"  data-tooltip="ffff">
   <span>+</span>
 </button>
-<button class="btn-m botonF4">
-  <span>+</span>
-</button>
-<button class="btn-m botonF5">
-  <span>+</span>
-</button>
-
+-->
 </div>
 @endsection
 <!-- Fin Section Contenido -->
@@ -89,15 +154,15 @@
 <!-- Inicio Section Modal -->
 
 @section('modal')
-<!-- Modal -->
-<div class="modal fade" id="login" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+<!-- Modal Alumnos -->
+<div class="modal fade" id="alumno" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
   <div class="modal-dialog" role="document">
     <div class="modal-content">
       <div class="modal-header">
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
           <span aria-hidden="true">&times;</span>
         </button>
-        <h3 class="modal-title colordiv" id="myModalLabel"> Inicia Sesión</h3>
+        <h3 class="modal-title colordiv" id="myModalLabel">Alumnos</h3>
       </div>
       <div class="modal-body">
         <center>
@@ -128,7 +193,49 @@
     </div>
   </div>
 </div>
-<!-- Modal Final -->
+<!-- Modal Final Alumnos -->
+
+<!-- Modal Unidad -->
+<div class="modal fade" id="unidad" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+        <h3 class="modal-title colordiv" id="myModalLabel">Unidad</h3>
+      </div>
+      <div class="modal-body">
+        <center>
+        <div class="logo1" >
+          <img src="img/logo.png" id="logo1" class="slideUp" alt="">
+        </div>
+        </center><br>
+
+       <div class="form-group has-default">
+        <center>
+          <label class="control-label" for="email">Email</label>
+          <input type="text" class="form-control" id="email">
+        </center>
+      </div>
+
+      <div class="form-group has-default">
+        <center>
+          <label class="control-label" for="pass">Contraseña</label>
+          <input type="password" class="form-control" id="pass">
+        </center>
+      </div>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Cancelar</button>
+        <button type="button" class="btn btn-primary" data-dismiss="modal">Acceder <span class="glyphicon glyphicon-log-in" aria-hidden="true"></span></button>
+        <!--<button type="button" class="btn btn-primary">Save changes</button>-->
+      </div>
+    </div>
+  </div>
+</div>
+<!-- Modal Final Unidad -->
+
 @endsection
 <!-- Fin Section Modal -->
 
